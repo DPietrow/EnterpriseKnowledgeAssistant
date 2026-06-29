1e2cf539d5c86f7c019593750b57fa28a561ee9b
DPietrow <davidpietrow17@gmail.com>
2026-06-28 22:17:09 -0400

    llmservice change

--- backend/services/llm_service.py
+++ backend/services/llm_service.py
@@ -1,14 +1,30 @@
 from openai import OpenAI
 import os
 
+
 class LLMService:
 
-    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    _client = None
+
+    @staticmethod
+    def get_client():
+        if LLMService._client is None:
+            api_key = os.getenv("OPENAI_API_KEY")
+
+            if not api_key:
+                raise Exception("OPENAI_API_KEY is missing")
+
+            LLMService._client = OpenAI(api_key=api_key)
+
+        return LLMService._client
+
 
     @staticmethod
     def generate(question, context):
 
-        response = LLMService.client.chat.completions.create(
+        client = LLMService.get_client()
+
+        response = client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
                 {
